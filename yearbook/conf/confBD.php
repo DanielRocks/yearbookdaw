06221bc3fd95bb0d116303bc5625d3e093d9cf71
DanielRocks <dan_aaraujo@hotmail.com>
2015-05-11 01:07:46 -0300

    confDB11
    Alteração completa da conexão com o bando de dados.

--- yearbook/conf/confBD.php
+++ yearbook/conf/confBD.php
@@ -1,19 +1,14 @@
 <?php
  function conn_mysql(){
 
-   $servidor = 'tcp:vf2yfajnje.database.windows.net';
-   $porta = 1433;
-   $banco = "mysql";
-   $usuario = "danielrocksu@vf2yfajnje";
-   $senha = "Yearbook2015";
-   
-      $conn = new PDO("mysql:host=$servidor;
-	                   port=$porta;
-					   dbname=$banco", 
-					   $usuario, 
-					   $senha,
-					   array(PDO::ATTR_PERSISTENT => true)
-					   );
-      return $conn;
+   try {
+	   $conn = new PDO ( "sqlsrv:server = tcp:vf2yfajnje.database.windows.net,1433; Database = mysql, danielrocksu, Yearbook2015");
+	   $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+	   }
+	   
+	   catch ( PDOException $e ) {
+		   print( "Error connecting to SQL Server." );
+		   die(print_r($e));
+		   }
    }
 ?>
