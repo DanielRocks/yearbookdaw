59b4611b893cbd30b991da8922d3a0d42ad92066
DanielRocks <dan_aaraujo@hotmail.com>
2015-05-11 01:40:56 -0300

    confDB20
    20th.

--- yearbook/conf/confBD.php
+++ yearbook/conf/confBD.php
@@ -7,7 +7,7 @@
    $usuario = "danielrocksu@vf2yfajnje";
    $senha = "Yearbook2015";
    
-      $conn = new PDO("sqlsrv:server = tcp:vf2yfajnje.database.windows.net,1433; Database = mysql", "danielrocksu@vf2yfajnje", "Yearbook2015",
+      $conn = new PDO("sqlsrv:server = tcp:vf2yfajnje.database.windows.net,1433; Database = mysql", "bce57962034b55", "ff0b7710",
 					   array(PDO::ATTR_PERSISTENT => true)
 					   );
       return $conn;
