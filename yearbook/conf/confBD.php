6ed9a2fa3544f9184f12fa31856d31e3e89106fd
DanielRocks <dan_aaraujo@hotmail.com>
2015-05-11 01:21:53 -0300

    confDB16
    Decima sexta tentativa.

--- yearbook/conf/confBD.php
+++ yearbook/conf/confBD.php
@@ -1,10 +1,10 @@
 <?php
  function conn_mysql(){
 
-   $servidor = 'br-cdbr-azure-south-a.cloudapp.net';
+   $servidor = 'vf2yfajnje.database.windows.net';
    $porta = 1433;
-   $banco = "yearbookdaw";
-   $usuario = "bce57962034b55";
+   $banco = "mysql";
+   $usuario = "danielrocksu@vf2yfajnje";
    $senha = "ff0b7710";
    
       $conn = new PDO("mysql:host=$servidor;
