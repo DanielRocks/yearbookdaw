edcc7f2ea0f5abf301b8cbf2df09557eb1441783
DanielRocks <dan_aaraujo@hotmail.com>
2015-05-11 20:19:36 -0300

    Correcao input.

--- yearbook/cadastroUsuario.php
+++ yearbook/cadastroUsuario.php
@@ -66,7 +66,7 @@ include_once("../modelos/cabecalho_login.html");
 			  
 			  <div class="form-group">
 				<label for="InputDescricao">Descrição:</label>
-				<textarea class="text form-control" id="InputDescricao" rows="4" name="descricao" placeholder="Escreva sobre você"></textarea>
+				<input type="text" class="form-control" id="InputDescricao" rows="4" name="descricao" placeholder="Escreva sobre você"></textarea>
 			  </div>
 			  
 			  <div class="form-group">
