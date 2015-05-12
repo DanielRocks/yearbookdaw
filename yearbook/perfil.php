67ea6c74486135a82a000c13dccaacd2717668c8
DanielRocks <dan_aaraujo@hotmail.com>
2015-05-11 22:09:43 -0300

    Proper photo size on profile

--- yearbook/perfil.php
+++ yearbook/perfil.php
@@ -48,7 +48,7 @@ try
 				
 				echo '<div class="row-fluid panel-body">';
 				echo '<div class="col-md-1"><strong>'. utf8_decode($contatosEncontrados['nomeCompleto']) .'</strong></div>';
-				echo '<div class="col-md-2"><img height="100" width="75" class="img-rounded" src="fotos/'. utf8_decode($contatosEncontrados['arquivoFoto']) .'"></img></div>';
+				echo '<div class="col-md-2"><img height="320" width="240" class="img-rounded" src="fotos/'. utf8_decode($contatosEncontrados['arquivoFoto']) .'"></img></div>';
 				echo '<div class="col-md-2"><strong>E-Mail:</strong><br> '. utf8_decode($contatosEncontrados['email']) .'</div>';
 				echo '<div class="col-md-2"><strong>Localidade:</strong><br>'. utf8_decode($contatosEncontrados['cidade']) . ' - ' . $contatosEncontrados['estado'] .'</div>';
 				echo '<div class="col-md-5"><strong>Descrição:</strong><br>'. utf8_decode($contatosEncontrados['descricao']) .'</div>';
